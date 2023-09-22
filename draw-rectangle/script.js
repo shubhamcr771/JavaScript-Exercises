5671a2f3c9311dd1efb67517cbc43312a54bdfb0
Shubham <shubhamsingh705010@gmail.com>
2023-09-22 13:23:12 +0530

    rectangle draw

---
+++ draw-rectangle/script.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const x = 50;
+const y = 60;
+const width = 100;
+const height = 275;
+const color = 'grey';
+
+function drawRectangle(canvas, x, y, width, height, color) {
+  const ctx = canvas.getContext("2d");
+  
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  // Draw the rectangle
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+}
+
+
+drawRectangle(canvas, x, y, width, height, color);
+
+
+    
