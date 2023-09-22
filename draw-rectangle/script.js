c1a0f411ddcfefda9c7f2b85f2bf260278f0fea7
Shubham <shubhamsingh705010@gmail.com>
2023-09-22 13:24:52 +0530

    format

--- draw-rectangle/script.js
+++ draw-rectangle/script.js
@@ -1,25 +1,21 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 const x = 50;
 const y = 60;
 const width = 100;
 const height = 75;
-const color = 'grey';
+const color = "grey";
 
 function drawRectangle(canvas, x, y, width, height, color) {
   const ctx = canvas.getContext("2d");
-  
+
   // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
   // Draw the rectangle
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 }
 
-
 drawRectangle(canvas, x, y, width, height, color);
-
-
-    
