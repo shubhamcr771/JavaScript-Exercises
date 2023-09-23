d92648cbc6a7e48c95a0837416ede69758f45a12
Shubham <shubhamsingh705010@gmail.com>
2023-09-23 14:25:29 +0530

    updated

--- event-bubbling/script.js
+++ event-bubbling/script.js
@@ -1,7 +1,15 @@
 const output = document.querySelector("#output");
+
 function handleClick(e) {
   output.textContent += `You clicked on a ${e.currentTarget.tagName} element\n`;
 }
 
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
+
+
+
+document.body.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick);
 container.addEventListener("click", handleClick);
+
