c2edacf4bd6a5c98baf455d5dcf904a496e7ed1a
Shubham <shubhamsingh705010@gmail.com>
2023-09-24 20:41:34 +0530

    name

--- object-basics/script.js
+++ object-basics/script.js
@@ -1,10 +1,11 @@
 const person = {
-  name: ["Bob", "Smith"],
+  name: {first:"Bob", 
+  last:"Smith"},
   age: 32,
-  bio: function () {
+  bio() {
     console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
   },
-  introduceSelf: function () {
+  introduceSelf() {
     console.log(`Hi! I'm ${this.name[0]}.`);
   },
 };
