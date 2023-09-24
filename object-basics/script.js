c724ebca854e16f2d055b8d6092d6211ff393a6f
Shubham <shubhamsingh705010@gmail.com>
2023-09-24 20:53:52 +0530

    update

--- object-basics/script.js
+++ object-basics/script.js
@@ -3,9 +3,14 @@ const person = {
   last:"Smith"},
   age: 32,
   bio() {
-    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.`);
   },
   introduceSelf() {
     console.log(`Hi! I'm ${this.name[0]}.`);
   },
+};
+person.name.last = "Singh";
+person["eye"] = "black";
+person.farewell = function() {
+  console.log("Bye Bye Shubham singh")
 };
