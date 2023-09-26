08d96079799ff808e2907ad3b054cad896f5e063
Shubham <shubhamsingh705010@gmail.com>
2023-09-26 13:39:43 +0530

    input

--- object 2/script.js
+++ object 2/script.js
@@ -1,11 +1,10 @@
 let bandInfo;
 
-    // Add your code here
+// Add your code here
 
+// Don't edit the code below here!
 
-    // Don't edit the code below here!
-
-    const section = document.querySelector('section');
-    let para1 = document.createElement('p');
-    para1.textContent = bandInfo;
-    section.appendChild(para1);
+const section = document.querySelector("section");
+let para1 = document.createElement("p");
+para1.textContent = bandInfo;
+section.appendChild(para1);
