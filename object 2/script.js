84a408586f21fc113d9ccee91db51f2fc36439af
Shubham <shubhamsingh705010@gmail.com>
2023-09-26 13:39:00 +0530

    add

---
+++ object 2/script.js
@@ -0,0 +1,11 @@
+let bandInfo;
+
+    // Add your code here
+
+
+    // Don't edit the code below here!
+
+    const section = document.querySelector('section');
+    let para1 = document.createElement('p');
+    para1.textContent = bandInfo;
+    section.appendChild(para1);
