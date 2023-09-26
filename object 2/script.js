c50e205f504998943b605ff488f540ac2d8ef6b7
Shubham <shubhamsingh705010@gmail.com>
2023-09-26 17:11:12 +0530

    band

--- object 2/script.js
+++ object 2/script.js
@@ -1,18 +1,28 @@
-let bandInfo = "This band is an Indian Classical and It first released his song in 2002";
+let bandInfo;
 
 // Add your code here
-const band = {
-    name: "Sarang",
-    nationality: "Indian",
-    genre: "Indian Classical",
-    members: 123002,
-    formed: 2002,
-    split: false,
-    albums: [
-        {[name: "Shu", released: 4004], [name: "sdkj", released: 8787]}
+const myFavoriteBand = {
+  name: "The Rockin' Tunes",
+  nationality: "United States",
+  genre: "Rock",
+  members: 4,
+  formed: 2005,
+  split: false,
+  albums: [
+    {
+      name: "First Harmony",
+      released: 2008,
+    },
+    {
+      name: "Rhythmic Roar",
+      released: 2012,
+    },
+  ],
+};
 
-    
-}
+ bandInfo = `${myFavoriteBand.name} is a ${myFavoriteBand.nationality} rock band formed in ${myFavoriteBand.formed}. The band consists of ${myFavoriteBand.members} members and is known for their energetic rock music. They have released several albums, with their debut album "${myFavoriteBand.albums[0].name}" being released in ${myFavoriteBand.albums[0].released}.`;
+
+
 
 // Don't edit the code below here!
 
