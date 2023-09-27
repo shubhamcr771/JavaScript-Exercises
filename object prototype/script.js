a68014196d4724545b9511834e1793e97343e277
Shubham <shubhamsingh705010@gmail.com>
2023-09-27 10:06:01 +0530

    s

--- object prototype/script.js
+++ object prototype/script.js
@@ -4,7 +4,7 @@ let profile = {
     name: "Shubham",
     age: 23,
     greet: function() {
-        s.textContent = `Hi everyone. I am ${profile.name}. I am ${profile.age} Years old.`;
+        s.textContent = `Hi everyone, I am ${profile.name}. I am ${profile.age} Years old.`;
 
     }
 }
