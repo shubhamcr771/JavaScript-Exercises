b2dd422eb89d3f25d29357cb6ffe50b7811565fb
Shubham <shubhamsingh705010@gmail.com>
2023-09-27 10:05:07 +0530

    proto

--- object prototype/script.js
+++ object prototype/script.js
@@ -1,7 +1,11 @@
+const s = document.querySelector("h1");
+
 let profile = {
     name: "Shubham",
     age: 23,
     greet: function() {
-        
+        s.textContent = `Hi everyone. I am ${profile.name}. I am ${profile.age} Years old.`;
+
     }
 }
+profile.greet();
