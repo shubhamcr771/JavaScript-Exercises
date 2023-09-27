edda44589089bb51400ca4820db9a9f12358080a
Shubham <shubhamsingh705010@gmail.com>
2023-09-27 10:21:24 +0530

    up

--- object prototype/script.js
+++ object prototype/script.js
@@ -1,11 +1,18 @@
 const s = document.querySelector("h1");
+const a = document.createAttribute("h1");
 
 let profile = {
     name: "Shubham",
     age: 23,
     greet: function() {
-        s.textContent = `Hi everyone, I am ${profile.name}. I am ${profile.age} Years old.`;
+        s.textContent = `Hi everyone, I am ${this.name}. I am ${this.age} Years old.`;
 
     }
-}
-profile.greet();
+};
+
+
+let profile1 = Object.create(profile);
+profile1.name = "Ankit";
+profile1.age = 20;
+profile1.greet(s);
+profile.greet(a);
